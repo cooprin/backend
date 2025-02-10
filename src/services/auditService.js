2d52bf87cb3d29af9dadbdfeb8a1c2366f679a27
Roman Tsyupryk <cooprin@gmail.com>
2025-02-10 14:02:04 +0100

    audit3

--- src/services/auditService.js
+++ src/services/auditService.js
@@ -1,5 +1,5 @@
 const { pool } = require('../database');
-const { ENTITY_TYPES, AUDIT_TYPES, AUDIT_LOG_TYPES } = require('../constants');
+const { ENTITY_TYPES, AUDIT_TYPES, AUDIT_LOG_TYPES } = require('../constants/constants');
 
 
 class AuditService {
