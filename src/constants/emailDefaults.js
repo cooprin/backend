7f5cabad87141bf2f96e8262723cae0f746df98b
cooprin <cooprin@gmail.com>
2025-07-11 16:08:14 +0200

    email13

--- src/constants/emailDefaults.js
+++ src/constants/emailDefaults.js
@@ -1,9 +1,9 @@
 const EMAIL_DEFAULTS = {
   // URL для особистого кабінету
-  portal_url: 'https://your-portal.example.com/portal',
+  portal_url: 'https://erp.cooprin.com.ua/',
   
   // Скільки днів додавати до терміну оплати
-  payment_due_days: 30
+  payment_due_days: 10
 }
 
 module.exports = { EMAIL_DEFAULTS };
